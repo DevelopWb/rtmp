28063046bb4e302c25865b5cbe68cedc17edae0e
wb <three6one@163.com>
2020-09-14 21:59:25 +0800

    公用注册码

--- RegMode/src/main/java/com/regmode/Utils/RegPubUtils.java
+++ RegMode/src/main/java/com/regmode/Utils/RegPubUtils.java
@@ -16,7 +16,7 @@ import java.util.Date;
  */
 public class RegPubUtils {
 
-    public static String PUBLIC_REGCODE = "jeC8Xml5";
+    public static String PUBLIC_REGCODE = "jeC8XmI5";
 
     // 判断网络是否正常
 
