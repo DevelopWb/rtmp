6483b6e453964ddcf21535bf65c0567db95248dc
wb <three6one@163.com>
2020-09-19 21:27:35 +0800

    2.0.12 添加通用注册码

--- RegMode/src/main/java/com/regmode/Utils/RegPubUtils.java
+++ RegMode/src/main/java/com/regmode/Utils/RegPubUtils.java
@@ -16,7 +16,7 @@ import java.util.Date;
  */
 public class RegPubUtils {
 
-    public static String PUBLIC_REGCODE = "jeC8XmI5";
+    public static String PUBLIC_REGCODE = "7stnMyTU";
 
     // 判断网络是否正常
 
