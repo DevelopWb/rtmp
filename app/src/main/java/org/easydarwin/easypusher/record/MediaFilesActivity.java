00146e36a5595cf628f450ae07c2e62c945d7702
tobato <three6one@163.com>
2020-10-12 14:44:50 +0800

    录像播放问题

--- app/src/main/java/org/easydarwin/easypusher/record/MediaFilesActivity.java
+++ app/src/main/java/org/easydarwin/easypusher/record/MediaFilesActivity.java
@@ -263,7 +263,7 @@ public class MediaFilesActivity extends AppCompatActivity implements Toolbar.OnM
                     intent.setAction(android.content.Intent.ACTION_VIEW);
                     if (Build.VERSION.SDK_INT >= 24) {//7.0 Android N
                         //com.xxx.xxx.fileprovider为上述manifest中provider所配置相同
-                        uri = FileProvider.getUriForFile(getContext(), "org.easydarwin.easyrtmp.fileProvider", f);
+                        uri = FileProvider.getUriForFile(getContext(), "org.chuangchi.yjzb.fileProvider", f);
                         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);//7.0以后，系统要求授予临时uri
                         // 读取权限，安装完毕以后，系统会自动收回权限，该过程没有用户交互
                     } else {//7.0以下
